b03facb6a9d0b6af0356568f37eac87256c52360
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-09 20:24:47 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,13 @@
-INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Chocolate', 'Caring', true);
+
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'Male', 'Ligth Brown', 'Intense', true);
+
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Yellow', 'Nice', true);
+
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'Male', 'Brown', 'Tough', true);
+
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'Female', 'Dark Brown', 'Nice', true);
+
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'Black', 'Though', true);
+
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'Female', 'Brown', 'Caring', true);
