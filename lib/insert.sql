696b8c9b4a40c311f76ea289593b83f2f7e52fb6
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-09 20:56:45 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Chocolate', 'Caring', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, 'Male', 'Ligth Brown', 'Intense', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Yellow', 'Nice', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'Male', 'Brown', 'Tough', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'Female', 'Dark Brown', 'Nice', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'Black', 'Though', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'Female', 'Brown', 'Caring', true);
+
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'Male', 'Chocolate', 'Caring', true);
